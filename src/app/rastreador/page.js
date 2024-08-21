6d9be62bb3cfe0726db04d9e15ba2a92d0a4457d
duarte25 <duarte.guga2025@gmail.com>
2024-08-21 14:18:05 -0400

    Apenas realizando mudanças suave na estilização do input date

--- src/app/rastreador/page.js
+++ src/app/rastreador/page.js
@@ -1,6 +1,5 @@
 "use client";
-// pages/index.js
-import Linha from '@/component/historyTracker';
+
 import { fetchApi } from '@/utils/fetchAPI';
 import dynamic from 'next/dynamic';
 import { useQuery } from "react-query";
@@ -17,15 +16,18 @@ export default function Home() {
 
   const [selectedVehicle, setSelectedVehicle] = useState(null);
   const [selectedDateInitial, setSelectedDateInitial] = useState(null)
+  const [selectedDateLast, setSelectedDateLast] = useState(null)
 
   // Converte selectedDateInitial para um objeto Date (opcional)
   const dateConvertInitial = new Date(selectedDateInitial);
+  const dateConvertLast = new Date(selectedDateLast);
 
   const { data, isLoading, isError, error, refetch } = useQuery({
-    queryKey: ['getVehicle', selectedVehicle, selectedDateInitial],
+    queryKey: ['getVehicle', selectedVehicle, selectedDateInitial, selectedDateLast],
     queryFn: async () => {
       const response = await fetchApi("/posicoes", "GET", {
         data_inicial: dateConvertInitial.toISOString(),
+        data_final: dateConvertLast.toISOString(),
         // data_final: "2024-08-14T19:00:30.000",
         serial: selectedVehicle
       });
@@ -46,14 +48,22 @@ export default function Home() {
     <div className='flex flex-row bg-purple-950'>
       <div className='w-3/12 flex flex-col items-center'>
         <h1 className='pt-64 text-3xl text-lime-400 font-semibold tracking-widest'>Rastreador Controle de Frota</h1>
-        <div className='pt-10 w-1/2'>
+        <div className='pt-10 w-1/2 flex flex-col gap-2'>
           <Combobox
             selectedVehicle={selectedVehicle}
             setSelectedVehicle={setSelectedVehicle}
           />
+
           <InputDate
-            selectedDateInitial={selectedDateInitial}
-            setSelectedDateInitial={setSelectedDateInitial} />
+            selectedDate={selectedDateInitial}
+            setSelectedDate={setSelectedDateInitial}
+            textPlacer={'Data inicial'} />
+
+          <InputDate
+            selectedDate={selectedDateLast}
+            setSelectedDate={setSelectedDateLast}
+            textPlacer={"Data final"} />
+
         </div>
       </div>
       <div className='w-9/12 justify-end'>
--- src/component/inputDate/index.jsx
+++ src/component/inputDate/index.jsx
@@ -7,8 +7,8 @@ import { Button } from '@/components/ui/button';
 import { Calendar } from '@/components/ui/calendar'; // Supondo que Calendar é de data
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 
-export default function InputDateTimeComponent({ selectedDateInitial, setSelectedDateInitial }) {
-  const [time, setTime] = useState(selectedDateInitial ? format(selectedDateInitial, 'HH:mm') : '00:00');
+export default function InputDateTimeComponent({ selectedDate, setSelectedDate, textPlacer }) {
+  const [time, setTime] = useState(selectedDate ? format(selectedDate, 'HH:mm') : '00:00');
 
   // Atualiza a data e hora selecionadas
   const handleDateSelect = (date) => {
@@ -16,18 +16,18 @@ export default function InputDateTimeComponent({ selectedDateInitial, setSelecte
       const newDateTime = new Date(date);
       newDateTime.setHours(parseInt(time.split(':')[0]));
       newDateTime.setMinutes(parseInt(time.split(':')[1]));
-      setSelectedDateInitial(newDateTime);
+      setSelectedDate(newDateTime);
     }
   };
 
   // Atualiza o horário selecionado
   const handleTimeChange = (event) => {
     setTime(event.target.value);
-    if (selectedDateInitial) {
-      const newDateTime = new Date(selectedDateInitial);
+    if (selectedDate) {
+      const newDateTime = new Date(selectedDate);
       newDateTime.setHours(parseInt(event.target.value.split(':')[0]));
       newDateTime.setMinutes(parseInt(event.target.value.split(':')[1]));
-      setSelectedDateInitial(newDateTime);
+      setSelectedDate(newDateTime);
     }
   };
 
@@ -37,27 +37,26 @@ export default function InputDateTimeComponent({ selectedDateInitial, setSelecte
         <Button
           variant="outline"
           className={cn(
-            'w-[280px] justify-start text-left font-normal',
-            !selectedDateInitial && 'text-muted-foreground'
+            'justify-start text-left font-normal',
+            !selectedDate && 'text-muted-foreground'
           )}
         >
           <CalendarIcon className="mr-2 h-4 w-4" />
           <span>
-            {selectedDateInitial
-              ? format(selectedDateInitial, 'PPPp') // 'PPPp' inclui a data e a hora
-              : 'Pick a date and time'}
+            {selectedDate
+              ? format(selectedDate, 'PPPp') // 'PPPp' inclui a data e a hora
+              : textPlacer}
           </span>
         </Button>
       </PopoverTrigger>
       <PopoverContent className="w-auto p-0">
         <Calendar
           mode="single"
-          selected={selectedDateInitial}
+          selected={selectedDate}
           onSelect={handleDateSelect}
           initialFocus
         />
-        <div className="flex items-center mt-2">
-          <ClockIcon className="mr-2 h-4 w-4" />
+        <div className="flex items-center justify-center mt-2">
           <input
             type="time"
             value={time}
