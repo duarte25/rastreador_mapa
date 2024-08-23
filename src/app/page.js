cae769ca2f4d38d0962ee5717700cd9cb78e84a6
duarte25 <duarte.guga2025@gmail.com>
2024-08-23 16:57:30 -0400

    Configurando o input para o mapa

--- src/app/page.js
+++ src/app/page.js
@@ -6,10 +6,15 @@ import { useQuery } from 'react-query';
 import Localization from '@/component/localization';
 import Combobox from '@/component/combobox';
 import Image from 'next/image';
+import { Popover, PopoverContent, PopoverTrigger, PopoverArrow } from '@/components/ui/popover';
+import { Button } from '@/components/ui/button';
+import { IoIosSearch } from "react-icons/io";
 
 const RealTimeTrackers = dynamic(() => import('@/component/realTimeTrackers'), { ssr: false });
 
 export default function Home() {
+  const [open, setOpen] = useState(true);
+
   const [selectedVehicle, setSelectedVehicle] = useState('Todos os rastreadores');
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ['getVehicle', selectedVehicle],
@@ -29,20 +34,43 @@ export default function Home() {
   const { location, error: locationError } = Localization();
 
   return (
-    <div className='flex flex-row'>
-      <div className='w-3/12 flex flex-col items-center'>
-        <h1 className='pt-64 text-3xl text-slate-800 font-semibold tracking-widest'>Rastreador Controle de Frota</h1>
-        <div className='pt-10 w-1/2'>
-          <Combobox 
-            selectedVehicle={selectedVehicle} 
-            setSelectedVehicle={setSelectedVehicle} 
-          />
-          <Image className='pt-48 w-48' width={100} height={100} src='./logo.svg' alt='logo' />
-        </div>
-      </div>
-      <div className='w-9/12 justify-end'>
+    <div className='relative w-full h-screen'>
+      <div className='absolute inset-0 z-10'>
         <RealTimeTrackers markers={markers} location={location} error={locationError} />
       </div>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            className={`flex fixed top-1 left-16 z-10 w-1/6 h-14 text-slate-800
+            ${open ? 'bg-white border-t-2 border-gray-500 rounded-t-3xl border-b border-transparent' : 'bg-white border border-gray-300 rounded-3xl'}`}
+            onClick={() => setOpen(!open)}
+          >
+            <div className='flex flex-row items-center gap-5'>
+              <a>Busque seu veículo...</a>
+              <IoIosSearch />
+            </div>
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent
+          className='z-[10] w-[--radix-popover-trigger-width] max-h-[--radix-popover-content-available-height] p-0 -mt-2 
+          h-48 flex flex-col gap-5 justify-center'
+          sideOffset={5}
+        >
+          <Combobox
+            selectedVehicle={selectedVehicle}
+            setSelectedVehicle={setSelectedVehicle}
+          />
+          <Combobox
+            selectedVehicle={selectedVehicle}
+            setSelectedVehicle={setSelectedVehicle}
+          />
+          <Combobox
+            selectedVehicle={selectedVehicle}
+            setSelectedVehicle={setSelectedVehicle}
+          />
+        </PopoverContent>
+      </Popover>
     </div>
+
   );
 }
